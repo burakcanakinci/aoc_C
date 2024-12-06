d682dad5e76651acc1a521743496c67f427e75ba
burakcanakinci <burakakinci.bca@gmail.com>
2024-12-06 10:30:27 +0200

    day 2 part 2

--- aoc/2024/day_2/main.c
+++ aoc/2024/day_2/main.c
@@ -5,28 +5,15 @@
 #define TEST_FILE "input.txt"
 #define MAX_LINE_LENGTH 1024
 
-int check_safe(char *lines) {
-    int numbers[8];
-    int i = 0;
-    char *token = strtok(lines, " ");
-
-    while (token != NULL) {
-        numbers[i++] = atoi(token);
-        token = strtok(NULL, " ");
-    }
-
-    // for (int j = 0; j < i; j++) {
-    //     printf("%d ", numbers[j]);
-    // }
-    // printf("\n");
-
+int check_safe(int *numbers, int index) {
     int is_increasing = 1;
     int is_decreasing = 1;
 
-    for (int j = 0; j < i - 1; j++) {
-        int diff = numbers[j+1] - numbers[j];
+    for (int i = 0; i < index - 1; i++) {
+        int diff = numbers[i+1] - numbers[i];
 
         if (diff > 3 || diff < -3 || diff == 0) {
+
             return 1;
         }
 
@@ -37,9 +24,41 @@ int check_safe(char *lines) {
         }
     }
 
+    return !(is_decreasing || is_increasing);
+}
+
+int check_removed_safe(char *lines) {
+    int numbers[8];
+    int index = 0;
+    char *token = strtok(lines, " ");
+
+    while (token != NULL) {
+        numbers[index++] = atoi(token);
+        token = strtok(NULL, " ");
+    }
+
+    if (check_safe(numbers, index) == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < index; i++) {
+        int temp[8];
+        int temp_index = 0;
+
+        for (int j = 0; j < index; j++) {
+            if (j != i) {
+                temp[temp_index++] = numbers[j];
+            }
+        }
+
+        if (check_safe(temp, temp_index) == 0) {
+            return 0;
+        }
+    }
+
     free(token);
 
-    return !(is_decreasing || is_increasing);
+    return 1;
 }
 
 int main(void) {
@@ -49,12 +68,13 @@ int main(void) {
     int safe = 0;
 
     while (fgets(line, sizeof(line), file) != NULL) {
-        if (check_safe(line) == 0) {
+        if (check_removed_safe(line) == 0) {
             safe++;
         }
     }
 
-    printf("\n%d\n", safe);
+    // printf("\nSafe: %d\n", safe);
+    printf("\nSafe with removed: %d\n", safe);
 
     fclose(file);
 
