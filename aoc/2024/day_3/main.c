599435e29e3d41ed152886cd426911b5c6777f0b
burakcanakinci <burakakinci.bca@gmail.com>
2024-12-11 15:00:10 +0200

    extracted mul's

--- aoc/2024/day_3/main.c
+++ aoc/2024/day_3/main.c
@@ -1,19 +1,15 @@
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pcre.h>
 #include <string.h>
 
 #define INPUT_FILE "input.txt"
 
 int main(void) {
-    FILE* file = fopen(INPUT_FILE, "r");
+    FILE *file = fopen(INPUT_FILE, "r");
 
-    const char *pattern = "mux\\([0-9]+,[0-9]+\\)";
-    const char *error;
-    int error_offset;
-    pcre *re;
-    int rc;
-    int output_vector[30];
+    regex_t regex;
+    const char *pattern = "mul\\([0-9]+,[0-9]+\\)";
 
     fseek(file, 0, SEEK_END);
     int file_size = ftell(file);
@@ -24,11 +20,38 @@ int main(void) {
     buffer[file_size] = '\0';
     fclose(file);
 
-    re = pcre_compile(pattern, 0, &error, &error_offset, NULL);
+    int ret = regcomp(&regex, pattern, REG_EXTENDED);
 
+    char *search_position = buffer;
+    regmatch_t match[1];
+    int match_count = 0;
 
+    while (1) {
+        ret = regexec(&regex, search_position, 1, match, 0);
+        if (ret == REG_NOMATCH) {
+            break; // no more matches
+        }
 
-    pcre_free(re);
+        int start = match[0].rm_so;
+        int end = match[0].rm_eo;
+
+        printf("%d: ",++match_count);
+        for (int i = start; i < end; i++) {
+            putchar(search_position[i]);
+        }
+        printf("\n");
+
+        size_t new_start = match[0].rm_eo;
+        memmove(buffer, buffer + new_start, strlen(buffer + new_start) + 1);
+
+        search_position += match[0].rm_eo;
+    } 
+
+    printf("total matches: %d", match_count);
+
+    /* printf("%s", buffer); */
+
+    regfree(&regex);
     free(buffer);
     return 0;
 }
