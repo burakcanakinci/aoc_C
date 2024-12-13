c14811db529a3c469f2067debb9b3a09d33baa7d
burakcanakinci <burakakinci.bca@gmail.com>
2024-12-13 15:52:17 +0200

    day 3 part 2

--- aoc/2024/day_3/main.c
+++ aoc/2024/day_3/main.c
@@ -1,59 +1,77 @@
-#include <regex.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define INPUT_FILE "input.txt"
-
-int mul(int a, int b) {
-    return a*b;
+// Function to extract a number from a string starting at a given index
+int extract_number(const char *str, int *index) {
+    int num = 0;
+    while (isdigit(str[*index])) {
+        num = num * 10 + (str[*index] - '0');
+        (*index)++;
+    }
+    return num;
 }
 
-int main(void) {
-    FILE *file = fopen(INPUT_FILE, "r");
+int process_text(const char *text_buffer) {
+    int sum = 0;
+    int mul_enabled = 1;
+    int i = 0;
+    int len = strlen(text_buffer);
+    
+    while (i < len) {
+        if (strncmp(&text_buffer[i], "do()", 4) == 0) {
+            mul_enabled = 1;
+            i += 4;
+        } else if (strncmp(&text_buffer[i], "don't()", 7) == 0) {
+            mul_enabled = 0;
+            i += 7;
+        } else if (strncmp(&text_buffer[i], "mul(", 4) == 0) {
+            i += 4; // Move past "mul("
+            if (mul_enabled) {
+                int num1 = extract_number(text_buffer, &i);
+                if (text_buffer[i] == ',') {
+                    i++; // Move past ","
+                    int num2 = extract_number(text_buffer, &i);
+                    if (text_buffer[i] == ')') {
+                        sum += num1 * num2; // Add the result of multiplication
+                        i++; // Move past ")"
+                    }
+                }
+            } else {
+                // Skip to the end of this invalid "mul" instruction
+                while (i < len && text_buffer[i] != ')') {
+                    i++;
+                }
+                if (text_buffer[i] == ')') {
+                    i++; // Move past ")"
+                }
+            }
+        } else {
+            i++; // Skip invalid characters
+        }
+    }
 
-    regex_t regex;
-    const char *pattern = "mul\\([0-9]+,[0-9]+\\)";
+    return sum;}
+
+int main() {
+    FILE *file = fopen("input.txt", "r");
 
     fseek(file, 0, SEEK_END);
-    int file_size = ftell(file);
+    long file_size = ftell(file);
     rewind(file);
 
     char *buffer = malloc(file_size + 1);
+
     fread(buffer, 1, file_size, file);
     buffer[file_size] = '\0';
+
     fclose(file);
 
-    int ret = regcomp(&regex, pattern, REG_EXTENDED);
+    int result = process_text(buffer);
+    printf("Result: %d", result);
 
-    char *search_position = buffer;
-    regmatch_t match[1];
-    int match_count = 0;
-    int total = 0;
-
-    while (1) {
-        ret = regexec(&regex, search_position, 1, match, 0);
-        char matched_str[20];
-
-        if (ret == REG_NOMATCH) {
-            break; // no more matches
-        }
-
-        int start = match[0].rm_so;
-        int end = match[0].rm_eo;
-        snprintf(matched_str, end - start + 1, "%s", search_position + start);
-        /* printf("%s\n", matched_str); */
-
-        int a, b;
-        if (sscanf(matched_str, "mul(%d,%d)", &a, &b) == 2) {
-            total += mul(a,b);
-        }
-
-        search_position += match[0].rm_eo;
-    } 
-
-    printf("total: %d", total);
-
-    regfree(&regex);
     free(buffer);
     return 0;
 }
+
